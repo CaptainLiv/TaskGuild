2689e674a49e1db14e795772d823e1cc7737a9c8
Ertan <ertan_ertas98@hotmail.com>
2024-04-11 10:36:41 +0200

    Prolfil.txt angelegt
    in der Profil Datei werden die Account daten erfasst

--- src/main/java/com/example/taskguild/Start_Application.java
+++ src/main/java/com/example/taskguild/Start_Application.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.File;
 
 public class Start_Application extends Application {
     @Override
@@ -17,7 +18,30 @@ public class Start_Application extends Application {
         stage.show();
     }
 
+    public static final String filepath_profil = "profil.txt";
+
+    public static boolean check_profil() {
+        File profildatei = new File(filepath_profil);
+        if (profildatei.isFile()) {
+            System.out.println("lade Profil");
+        } else {
+            try{
+               if(profildatei.createNewFile()){
+                   System.out.println("Datei wurde angelegt");
+               }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            System.out.println("erstell Profil");
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+       // if (check_profil()) {
+       //     System.out.println("exist");
+       // }
         launch();
     }
 }
